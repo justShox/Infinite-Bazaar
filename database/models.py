21c50b205bbfcff5ecdddf140c0ea2fdb7bad747
justShox <147226861+justShox@users.noreply.github.com>
2024-04-02 19:53:38 +0500

    Finished

--- database/models.py
+++ database/models.py
@@ -7,11 +7,11 @@ from database import base
 class User(base):
     __tablename__ = 'users'
     user_id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String)
-    surname = Column(String)
-    phone_number = Column(String, unique=True)
-    e_mail = Column(String, unique=True)
-    password = Column(String)
+    username = Column(String, nullable=False)
+    surname = Column(String, nullable=False)
+    phone_number = Column(String, unique=True, nullable=False)
+    e_mail = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
     points = Column(Integer, default=100)
     products_bought = Column(Integer, default=0)
     created_at = Column(DateTime)
@@ -20,9 +20,9 @@ class User(base):
 # Таблица продукта
 class Products(base):
     __tablename__ = 'products'
-    product_id = Column(Integer, primary_key=True, autoincrement=True)
-    product_name = Column(String, unique=True)
-    product_des = Column(String)
+    product_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    product_name = Column(String, unique=True, nullable=False)
+    product_des = Column(String, nullable=False)
     product_quantity = Column(Integer, default=10)
     product_price = Column(Float, default=10)
     added_at = Column(DateTime)
@@ -31,8 +31,8 @@ class Products(base):
 # Для добавления фото продукта
 class ProductPhoto(base):
     __tablename__ = 'product_photos'
-    product_id = Column(Integer, ForeignKey('products.product_id'))
-    photo_id = Column(Integer, primary_key=True, autoincrement=True)
+    product_id = Column(Integer, ForeignKey('products.product_id'), nullable=False)
+    photo_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     photo_path = Column(String)
 
     product_fk = relationship(Products, lazy='subquery')
@@ -41,10 +41,10 @@ class ProductPhoto(base):
 # Таблица для отзывов
 class Review(base):
     __tablename__ = 'reviews'
-    review_id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.user_id'))
-    product_id = Column(Integer, ForeignKey('products.product_id'))
-    review_text = Column(Text)
+    review_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    product_id = Column(Integer, ForeignKey('products.product_id'), nullable=False)
+    review_text = Column(Text, nullable=False)
     posted_at = Column(DateTime)
 
     user_fk = relationship(User, lazy='subquery')
